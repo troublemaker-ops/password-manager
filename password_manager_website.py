9f12e1eade76f2c341f9d62dd4ff25b8c2700e05
troublemaker-ops <jinjeyt@gmail.com>
2025-09-17 22:35:07 +0800

    Update password_manager_website.py

--- password_manager_website.py
+++ password_manager_website.py
@@ -1,88 +1,92 @@
 import sqlite3
-import string,secrets
+import string, secrets
 import streamlit as st
 
 st.title("Password Manager")
 
-conn=sqlite3.connect("password_manager.db")
-c=conn.cursor()
-#for the log in system
+conn = sqlite3.connect("password_manager.db")
+c = conn.cursor()
 
 
-def read_data(find,table,where,what):
-    c.execute(f"SELECT {find} FROM {table} WHERE {where}=?",(what,))
-    rows=c.fetchone()
+# for the log in system
+
+
+def read_data(find, table, where, what):
+    c.execute(f"SELECT {find} FROM {table} WHERE {where}=?", (what,))
+    rows = c.fetchone()
     if rows:
         return rows
     else:
         return False
 
-def store_data(table,service,name,password):
+
+def store_data(table, service, name, password):
     c.execute(f"""CREATE TABLE IF NOT EXISTS {table}(
     {service}  TEXT PRIMARY KEY ,
     password TEXT)""")
-    c.execute(f"""INSERT OR REPLACE INTO {table}({service},password) VALUES (?,?)""",(name,password))
+    c.execute(f"""INSERT OR REPLACE INTO {table}({service},password) VALUES (?,?)""", (name, password))
     conn.commit()
 
-def stored_password(table_name):
 
+def stored_password():
     name = st.text_input("please enter app or website name:")
     if not name:
         return
-    data=read_data("*",table_name,"app",name)
+    data = read_data("*", table_name, "app", name)
     if not data:
-        generate = st.radio("do you want system generate password for you?:",["yes","no"])
+        generate = st.radio("do you want system generate password for you?:", ["yes", "no"])
         if generate.startswith("y"):
             if st.button("generate password"):
                 password = generate_password()
                 st.session_state.generated_password = password
 
         if "generated_password" in st.session_state:
-            password=st.session_state.generated_password
-            satisfy = st.radio(f"generated password is: {password}.Do you satisfy?",["yes","no"])
+            password = st.session_state.generated_password
+            satisfy = st.radio(f"generated password is: {password}.Do you satisfy?", ["yes", "no"])
             if satisfy.startswith("y"):
-                store_data(table_name,"app",name,password)
+                store_data(table_name, "app", name, password)
                 st.success("password saved")
                 del st.session_state.generated_password
             else:
                 st.info("click 'generate password' to generate new password")
         else:
-            password = st.text_input("please enter app password:",type="password").replace(" ", "")
+            password = st.text_input("please enter app password:", type="password").replace(" ", "")
             if password:
-                store_data(table_name,"app",name,password)
+                store_data(table_name, "app", name, password)
                 st.success("password saved")
 
     else:
         st.warning("password already exists.if you want to change it please choose option change password")
 
-def check_password(table_name):
-    name=st.text_input("please enter the app or website name:")
+
+def check_password():
+    name = st.text_input("please enter the app or website name:")
     if not name:
         return
-    value=read_data("password",table_name,"app",name)
+    value = read_data("password", table_name, "app", name)
     if value:
-        st.text_input(f"The password of {name} is: {value[0]}",type="password")
+        st.text_input(f"The password of {name} is: {value[0]}", type="password")
     else:
         st.write(f"The password of {name} have not save yet")
 
-def change_password(table_name):
 
-    name=st.text_input("please enter app or website name that you want to change:")
-    data=read_data("password",table_name,"app",name)
+def change_password():
+    name = st.text_input("please enter app or website name that you want to change:")
+    data = read_data("password", table_name, "app", name)
     if data:
-        generate=st.radio("do you want system generate password for you?",["yes","no"])
+        generate = st.radio("do you want system generate password for you?", ["yes", "no"])
 
         if generate.startswith("y"):
             if st.button("generate password"):
-                    password = generate_password()
-                    st.session_state.generated_password = password
+                password = generate_password()
+                st.session_state.generated_password = password
 
             if "generated_password" in st.session_state:
                 password = st.session_state.generated_password
                 satisfy = st.radio(f"generated password is: {password}.Do you satisfy?", ["yes", "no"])
                 if satisfy.startswith("y"):
 
-                    store_data(table_name,"app",name,password)
+                    store_data(table_name, "app", name, password)
                     st.success("password saved")
                     del st.session_state.generated_password
                 else:
@@ -90,26 +94,28 @@ def change_password(table_name):
             else:
                 password = st.text_input("please enter app password:", type="password").replace(" ", "")
                 if password:
-                   data[name] = password
-                   store_data(table_name,"app",name,password)
-                   st.success("password saved")
+                    data[name] = password
+                    store_data(table_name, "app", name, password)
+                    st.success("password saved")
 
     else:
         st.write(f"The password of {name} have not save yet")
 
+
 def generate_password():
-    length=st.slider("please enter the length of password:",4,15,8)
-    strength=st.radio("please enter the strength of password(weak/medium/strong):",
-                      ["digit only (weak)","digit + letter (medium)","digit + letter +symbol (strong)"])
+    length = st.slider("please enter the length of password:", 4, 15, 8)
+    strength = st.radio("please enter the strength of password(weak/medium/strong):",
+                        ["digit only (weak)", "digit + letter (medium)", "digit + letter +symbol (strong)"])
     if strength.startswith("weak"):
-        character=string.digits
+        character = string.digits
     elif strength.startswith("medium"):
-        character=string.ascii_letters+string.digits
+        character = string.ascii_letters + string.digits
     else:
-        character=string.ascii_letters+string.digits+string.punctuation
+        character = string.ascii_letters + string.digits + string.punctuation
     return ''.join(secrets.choice(character) for _ in range(int(length)))
 
-lock_table="username_password"
+
+lock_table = "username_password"
 
 c.execute("""CREATE TABLE IF NOT EXISTS username_password(
 username TEXT PRIMARY KEY,
@@ -126,46 +132,47 @@ else:
     # --- SIGN IN ---
     if register.startswith("already"):
         username = st.text_input("please enter username:")
-        password = st.text_input("please enter password:", type="password")
+        password_lock = st.text_input("please enter password:", type="password")
 
-        if st.button("Sign In"):
+        if st.button("sign in"):
             lock = read_data("password", lock_table, "username", username)
-                
-            if lock and password == lock[0]:
+
+            if lock and password_lock == lock[0]:
                 st.session_state.logged_in = True
                 st.session_state.username = username
                 st.success("You are logged in")
-                
+
             else:
                 st.error("Your password or username is wrong. Please try again")
-                    
+
 
     # --- SIGN UP ---
     else:
         username = st.text_input("please enter username for your account:")
-        password = st.text_input("please enter password(remember for the future logins):", type="password")
+        password_lock = st.text_input("please enter password(remember for the future logins):", type="password")
 
         if st.button("Sign Up"):
-            if not username or not password:
+            if not username or not password_lock:
                 st.error("Username and password cannot be empty")
             else:
                 lock = read_data("password", lock_table, "username", username)
                 if lock:
                     st.error("This username already exists")
                 else:
-                    store_data(lock_table, "username", username, password)
+                    store_data(lock_table, "username", username, password_lock)
                     st.success("Your account has been created successfully. Please sign in again")
                     st.stop()
-
+        else:
+            st.stop()
 # ---------------------- PASSWORD TABLE ----------------------
-
+table_name = f"password_{st.session_state.username}"
 if "logged_in" in st.session_state and st.session_state.logged_in:
-    table_name = f"password_{st.session_state.username}"
+    
     c.execute(f"""CREATE TABLE IF NOT EXISTS {table_name}(
     app TEXT PRIMARY KEY,
     password TEXT)""")
 
-#want=want to stored password or no
+# want=want to stored password or no
 if "mode" not in st.session_state:
     st.session_state.mode = "menu"
 
@@ -178,16 +185,15 @@ if st.session_state.mode == "menu":
     )
 
 # Handle actions
-if table_name:
-    if st.session_state.mode == "store password":
-        stored_password(table_name)
-    elif st.session_state.mode == "check password":
-        check_password(table_name)
-    elif st.session_state.mode == "change password":
-        change_password(table_name)
-    elif st.session_state.mode == "exit":
-        st.write("Thank you for using this program")
-        st.stop()
+if st.session_state.mode == "store password":
+    stored_password()
+elif st.session_state.mode == "check password":
+    check_password()
+elif st.session_state.mode == "change password":
+    change_password()
+elif st.session_state.mode == "exit":
+    st.write("Thank you for using this program")
+    st.stop()
 
 # Navigation
 if st.session_state.mode != "exit":
@@ -195,6 +201,7 @@ if st.session_state.mode != "exit":
         st.session_state.mode = "menu"
 
 conn.close()
+
 
 
 
